6dce47a9c809876c9bc721e48139993690e204d8
kmo <mohankapil@gmail.com>
2020-04-01 21:20:27 +0100

    Chit chat server interim commit

---
+++ engine/chit-chat-server/src/main/java/com/mcl/chit/chat/engine/ChitChatServer.java
@@ -0,0 +1,13 @@
+package com.mcl.chit.chat.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChitChatServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChitChatServer.class, args);
+    }
+
+}
