674d8fbe72ddafdfeb3a235e3e44185020cd1ee0
kmo <mohankapil@gmail.com>
2020-04-02 23:04:56 +0100

    Upgraded source level to Java 11, added test dependencies, added integration test placeholder

---
+++ engine/chit-chat-server/src/test/java/com/mcl/chit/chat/engine/ChitChatServerIntegrationTest.java
@@ -0,0 +1,17 @@
+package com.mcl.chit.chat.engine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ChitChatServer.class)
+public class ChitChatServerIntegrationTest {
+
+    @Test
+    public void shouldStartContainer() {
+        //TODO add actual meat, its fake
+    }
+
+}
