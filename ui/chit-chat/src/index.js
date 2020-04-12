63954400f41cb8e6190e70f928e5c45c4dcdd505
kmo <mohankapil@gmail.com>
2020-04-12 18:09:56 +0100

    #3 Added base 'reactjs' project - interim commit for 'Join Chat' feature.

---
+++ ui/chit-chat/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Dialogue from './app/join-chat.js';
+
+ReactDOM.render(
+  <Dialogue />,
+  document.getElementById('root')
+);
+
