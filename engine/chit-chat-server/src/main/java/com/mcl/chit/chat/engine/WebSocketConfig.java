d6481cbd46bdcf3616ea47746d30dcde4397341a
kmo <mohankapil@gmail.com>
2020-05-17 21:53:17 +0100

    #26 Exposed environment variable to configure allowed origins

--- engine/chit-chat-server/src/main/java/com/mcl/chit/chat/engine/WebSocketConfig.java
+++ engine/chit-chat-server/src/main/java/com/mcl/chit/chat/engine/WebSocketConfig.java
@@ -1,23 +1,38 @@
 package com.mcl.chit.chat.engine;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.concurrent.DefaultManagedTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.StompWebSocketEndpointRegistration;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.logging.Logger;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    private static final Logger LOGGER = Logger.getLogger(WebSocketConfig.class.getName());
+
     public static final String WS_ENDPOINT = "/chit-chat";
     public static final String APP_ENDPOINT_PREFIX = "/app";
     public static final String BROADCAST_TOPIC = "/topic/chit-chat";
 
+    @Value("${APP_DOMAIN}")
+    private String appDomain;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint(WS_ENDPOINT).withSockJS();
+        StompWebSocketEndpointRegistration endpointRegistration = registry.addEndpoint(WS_ENDPOINT);
+        LOGGER.info(String.format("APP_DOMAIN env variable resolved to %s", appDomain));
+        if (appDomain != null && !appDomain.isBlank()) {
+            // Add support for application running on SSL
+            endpointRegistration.setAllowedOrigins(appDomain + ":443");
+        }
+        endpointRegistration.withSockJS();
     }
 
     @Override
