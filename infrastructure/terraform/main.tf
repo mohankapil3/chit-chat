8ac6a46207df90a58b2c5a94785e6b39df8c3722
kmo <mohankapil@gmail.com>
2020-05-18 17:36:13 +0100

    #26 Added steps to build AWS Beanstalk environment and deploy application from scratch

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -79,7 +79,7 @@ resource "aws_elastic_beanstalk_environment" "chit-chat" {
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
     name = "CHIT_CHAT_ALLOWED_ORIGINS"
-    value = "*"
+    value = "*" # This is unsafe, should only contain allowed absolute origins
   }
 }
 
