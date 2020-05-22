1756a0e669951999dd2c9855834fb88518f4810a
kmo <mohankapil@gmail.com>
2020-05-22 08:40:28 +0100

    #19 Refactored test to use module testing-library/react

--- client/chit-chat/src/__tests__/chat-header.test.js
+++ client/chit-chat/src/__tests__/chat-header.test.js
@@ -3,13 +3,16 @@ import { render, cleanup } from '@testing-library/react';
 import ChatHeader from '../app/chat-header.js';
 
 it("ChatHeader renders with status prop", () => {
+    // Arrange
     const status = {
         text: "Some Status",
         style: "Some Style"
     };
 
+    // Act
     const { getByText } = render(<ChatHeader status={ status } />);
 
+    // Assert
     const appHeader = getByText("ChitChat");
     expect(appHeader).toBeDefined();
 
--- client/chit-chat/src/__tests__/chat-room.test.js
+++ client/chit-chat/src/__tests__/chat-room.test.js
@@ -1,48 +1,35 @@
 import React from "react";
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
+import { render, cleanup } from '@testing-library/react';
 import ChatRoom from '../app/chat-room.js';
 
-let container = null;
-beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement("div");
-  document.body.appendChild(container);
+it("ChatRoom renders with user summary, message text box and past messages", () => {
+    // Arrange
+    const message1 = { sender: "user1", content: "Hello from user1"};
+    const message2 = { sender: "user2", content: "Hello from user2"};
+    const messages = [message1, message2];
+    const status = { text: "Connected", style: "style1" };
+
+    // Act
+    const { getByText, getByPlaceholderText } = render(
+        <ChatRoom chatName="user3"
+                status = { status }
+                messages={ messages }
+                onSendMessage= { ()=> {} }
+                onDoLogout= { ()=> {} } />
+    );
+
+    // Assert
+    const userSummary = getByText("Your chat name - user3");
+    expect(userSummary).toBeDefined();
+
+    const messageBox = getByPlaceholderText("Enter message...");
+    expect(messageBox).toBeDefined();
+
+    const message1Container = getByText("Hello from user1");
+    expect(message1Container).toBeDefined();
+
+    const message2Container = getByText("Hello from user2");
+    expect(message2Container).toBeDefined();
 });
 
-it("renders ChatRoom with some dummy user and messages", () => {
-  const message1 = { sender: "user1", content: "Hello from user1"};
-  const message2 = { sender: "user2", content: "Hello from user2"};
-  const messages = [message1, message2];
-  const status = { text: "Connected", style: "style1" };
-
-  act(() => {
-    render(<ChatRoom chatName="user3"
-                     status = { status }
-                     messages={ messages }
-                     onSendMessage= { ()=> {} }
-                     onDoLogout= { ()=> {} } />
-    , container);
-  });
-
-  const componentWrapper = container.children.item(0);
-
-  const chatHeaderWrapper = componentWrapper.children.item(0);
-  expect(chatHeaderWrapper.textContent).toEqual("ChitChatStatus - Connected");
-
-  const userNameWrapper = componentWrapper.children.item(1);
-  expect(userNameWrapper.textContent).toEqual("Your chat name - user3");
-
-  const chatMessage1 = componentWrapper.children.item(3);
-  expect(chatMessage1.textContent).toEqual("user1: Hello from user1");
-
-  const chatMessage2 = componentWrapper.children.item(4);
-  expect(chatMessage2.textContent).toEqual("user2: Hello from user2");
-});
-
-afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
+afterEach(cleanup);
